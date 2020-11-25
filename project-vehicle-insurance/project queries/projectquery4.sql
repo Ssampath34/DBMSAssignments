68ef3cc0555fb00eee6807c9b3e6ea95f59719f7
Sakala Sampath <53968350+Ssampath34@users.noreply.github.com>
2020-11-25 13:49:26 +0530

    Delete projectquery4.sql

--- project-vehicle-insurance/project queries/projectquery4.sql
+++
@@ -1,3 +0,0 @@
-SELECT c.* FROM g2customer as c JOIN g2vehicle AS v ON c.Cust_Id=v.Cust_Id JOIN g2premium_payment AS g
-ON g.Cust_Id =v.Cust_Id JOIN g2incident_report AS i ON i.Cust_Id=g.Cust_Id AND g.Premium_Payment_Amount=0 AND i.Incident_Type='accident'
- GROUP BY i.Cust_Id HAVING count(i.Cust_Id)>1
