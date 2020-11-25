56513b8b96298a33aac9ea129daab6be47f33374
Sakala Sampath <53968350+Ssampath34@users.noreply.github.com>
2020-11-25 13:49:18 +0530

    Delete projectquery3.sql

--- project-vehicle-insurance/project queries/projectquery3.sql
+++
@@ -1,5 +0,0 @@
-
-SELECT i.* FROM g2insurance_company AS i JOIN  g2product as p
-on p.Company_Name=i.Company_Name group by p.Company_Name having count(*)>
-ALL(select count(d.Department_Name) from g2insurance_company as r join g2department as d on r.Company_Name=d.Company_Name  group by 
-d.Department_Name having count(d.Department_Name)and count(r.Company_Location)>1)
